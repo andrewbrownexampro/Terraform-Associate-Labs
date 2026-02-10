a76345c70fccda5bcab1c1d46e62e319adb747ab
Andrew Brown <andrewbrownexamprotraining@gmail.com>
2026-02-10 09:21:01 -0500

    Add create_before_destroy lab

--- 055_lifecycle-create-before-destroy/main.tf
+++ 055_lifecycle-create-before-destroy/main.tf
@@ -7,10 +7,6 @@ terraform {
   }
 
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
     random = {
       source  = "hashicorp/random"
       version = "~> 3.0"
@@ -18,33 +14,14 @@ terraform {
   }
 }
 
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-cbd-demo"
-  location = "East US"
-}
-
-resource "random_string" "suffix" {
-  length  = 5
-  special = false
-  upper   = false
-}
-
-resource "azurerm_public_ip" "pip" {
-  name                = "pip-cbd-${random_string.suffix.result}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-  sku = "Standard" 
+resource "random_pet" "example" {
+  length = 2
 
   lifecycle {
     create_before_destroy = true
   }
 }
 
-output "pip_name" {
-  value = azurerm_public_ip.pip.name
+output "pet_name" {
+  value = random_pet.example.id
 }
