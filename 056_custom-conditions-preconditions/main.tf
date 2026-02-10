5378e68ba509ddc63fc68449ff1e3a6ae0730a1d
Andrew Brown <andrewbrownexamprotraining@gmail.com>
2026-02-10 15:32:59 -0500

    testing preconditions

---
+++ 056_custom-conditions-preconditions/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  cloud {
+    organization = "examprotraining"
+    workspaces {
+      name = "lab-preconditions"
+    }
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+  default     = "prod" # intentionally wrong to demonstrate precondition failure
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-precondition-demo"
+  location = "East US"
+
+  lifecycle {
+    precondition {
+      condition     = var.environment == "dev"
+      error_message = "Environment must be 'dev' to create this resource group."
+    }
+  }
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
