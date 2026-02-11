669c88363b23dc19ded12fde6c78cb301cf7e868
Andrew Brown <andrewbrownexamprotraining@gmail.com>
2026-02-11 10:22:58 -0500

    Add input variable validation lab

--- 058_input-variable-validation/main.tf
+++ 058_input-variable-validation/main.tf
@@ -6,19 +6,17 @@ terraform {
     }
   }
 
+  required_version = ">= 1.2.0"
+
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
+    random = {
+      source  = "hashicorp/random"
       version = "~> 3.0"
     }
   }
 }
 
-provider "azurerm" {
-  features {}
-}
-
-# Allowed values validation 
+# Allowed values validation
 variable "environment" {
   description = "Deployment environment"
   type        = string
@@ -30,11 +28,11 @@ variable "environment" {
   }
 }
 
-# Format validation 
+# Format validation
 variable "resource_group_name" {
   description = "Name of the resource group (must start with rg-)"
   type        = string
-  default     = "resource-group-demo" # intentionally wrong test
+  default     = "resource-group-demo" # intentionally wrong
 
   validation {
     condition     = can(regex("^rg-[a-z0-9-]{3,20}$", var.resource_group_name))
@@ -42,11 +40,11 @@ variable "resource_group_name" {
   }
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = "East US"
+resource "random_pet" "rg" {
+  prefix = var.resource_group_name
+  length = 2
 }
 
 output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
+  value = random_pet.rg.id
 }
