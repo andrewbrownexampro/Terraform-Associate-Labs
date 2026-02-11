0f0b8cb781b444def69d5c69e4132f2079d04dc4
Andrew Brown <andrewbrownexamprotraining@gmail.com>
2026-02-10 19:13:40 -0500

    Add postconditions lab

---
+++ 057_custom-conditions-postconditions/main.tf
@@ -0,0 +1,46 @@
+terraform {
+  cloud {
+    organization = "examprotraining"
+    workspaces {
+      name = "lab-postconditions"
+    }
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Intentionally set wrong first to demonstrate failure
+variable "rg_location" {
+  description = "Azure region for the resource group"
+  type        = string
+  default     = "West US 2"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-postcondition-demo"
+  location = var.rg_location
+
+  lifecycle {
+    postcondition {
+      condition     = self.location == "eastus"
+      error_message = "Postcondition failed: resource group must be in East US (eastus)."
+    }
+  }
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "resource_group_location" {
+  value = azurerm_resource_group.rg.location
+}
